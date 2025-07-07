ce62e46c2e9a8081f950d3e324f39ac0c876fb7c
coral127 <sasahappy90@gmail.com>
2025-07-07 19:20:50 +0900

    Week11-1
    Fin

--- Week11/Assets/Scripts/MoveLeft.cs
+++ Week11/Assets/Scripts/MoveLeft.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,7 @@ public class MoveLeft : MonoBehaviour
 {
     private float speed = 30f;
     private PlayerMove PlayerMoveScript;
+    private float leftBound = -15;
     void Start()
     {
         PlayerMoveScript = GameObject.Find("Player").GetComponent<PlayerMove>();
@@ -17,6 +19,11 @@ public class MoveLeft : MonoBehaviour
         if(PlayerMoveScript.gameOver == false)
         {
             transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if(transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
         }
         
     }
--- Week11/Assets/Scripts/PlayerMove.cs
+++ Week11/Assets/Scripts/PlayerMove.cs
@@ -9,20 +9,33 @@ public class PlayerMove : MonoBehaviour
     public float jumpForce = 10f;
     public bool isOnGround = true;
     public bool gameOver;
+    private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource playerAudio;
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
+        playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+
+            
         }
     }
     private void OnCollisionEnter(Collision collision)
@@ -32,11 +45,18 @@ public class PlayerMove : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            dirtParticle.Play();
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
+            playerAudio.PlayOneShot(crashSound, 1f);
             Debug.Log("Game Over");
             gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
+
         }
     }
 }
